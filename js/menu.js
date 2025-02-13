24e4f010ba841f6f0db7014ff7017c1338f76997
Danil-27 <pde200126@gmail.com>
2025-02-13 23:42:10 +0300

    feature: menu

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+const html = document.querySelector('html');
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.navbar');
+const links = document.querySelectorAll('.menu__link');
+
+console.log(html);
+burger.addEventListener('click', toggleNavbar);
+
+links.forEach(link => {
+	link.addEventListener('click', toggleNavbar);
+});
+
+function toggleNavbar() {
+	if (burger.classList.contains('burger')) {
+		burger.classList.toggle('burger-active');
+		navbar.classList.toggle('navbar-active');
+		html.classList.toggle('noscroll');
+	}
+}
